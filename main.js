42940f8a9a993d123a5007235e9f701bbc170444
Parthiv7890 <88973956+Parthiv7890@users.noreply.github.com>
2022-10-01 16:20:23 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ let lwy = 0
 let rwx = 0
 let rwy = 0
 let scorelw = 0
+let scorerw = 0
 let song1status 
 let song2status 
 function preload(){
@@ -30,7 +31,8 @@ function gotResults(results){
         lwx = results[0].pose.leftWrist.x
         lwy = results[0].pose.leftWrist.y
 
-        scorelw = results[0].keypoints[9].score
+        scorelw = results[0].pose.keypoints[9].score
+        scorerw = results[0].pose.keypoints[10].score
 
 
     }
@@ -44,12 +46,21 @@ function draw(){
     stroke("#FF0000")
 
     song1status = song1.isPlaying()
+    song2status = song2.isPlaying()
     if(scorelw > 0.2){
         circle(lwx,lwy,20)
         song2.stop()
         if(song1status == false){
             song1.play()
             document.getElementById("songplay").innerHTML = "Carrol of the Bells"
+        }
+    }
+    if(scorerw>0.2){
+        circle(rwx,rwy,20)
+        song1.stop()
+        if(song2status == false){
+            song2.play()
+            document.getElementById("songplay").innerHTML = "Hedwig's Theme"
         }
     }
 }
